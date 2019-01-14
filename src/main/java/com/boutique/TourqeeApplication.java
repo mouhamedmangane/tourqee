87840acecabd9ffda8f4df79c213594a13cc9ead
mouhamedmangane <mouhamedmangane91@gmail.com>
2019-01-14 19:27:12 +0000

    one

---
+++ src/main/java/com/boutique/TourqeeApplication.java
@@ -0,0 +1,14 @@
+package com.boutique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourqeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourqeeApplication.class, args);
+	}
+
+}
+
