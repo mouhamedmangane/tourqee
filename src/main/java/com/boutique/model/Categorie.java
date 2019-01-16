b1ce79f9066a6dd082d2bc79e716b62fa3719edc
unknown <anonymeus10@gmail.com>
2019-01-16 11:00:14 +0100

    creation des objets Collection,Categorie,Model

---
+++ src/main/java/com/boutique/model/Categorie.java
@@ -0,0 +1,76 @@
+package com.boutique.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Categorie {
+	
+	@Id
+	@GeneratedValue
+	private long idCategorie;
+
+	@Column(name="nom")
+	private String nom;
+	
+	@Column(name="date")
+	private Date date;
+	
+	private List<Collection> collection;
+
+	public Categorie() {
+		super();
+	}
+
+	public Categorie(long idCategorie, String nom, Date date, List<Collection> collection) {
+		super();
+		this.idCategorie = idCategorie;
+		this.nom = nom;
+		this.date = date;
+		this.collection = collection;
+	}
+
+	public long getIdCategorie() {
+		return idCategorie;
+	}
+
+	public void setIdCategorie(long idCategorie) {
+		this.idCategorie = idCategorie;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public List<Collection> getCollection() {
+		return collection;
+	}
+
+	public void setCollection(List<Collection> collection) {
+		this.collection = collection;
+	}
+
+	@Override
+	public String toString() {
+		return "Categorie [idCategorie=" + idCategorie + ", nom=" + nom + ", date=" + date + ", collection="
+				+ collection + "]";
+	}
+
+}
