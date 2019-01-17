59056fd0978d0adc6498e3190d8b4bf6c0f16c12
unknown <anonymeus10@gmail.com>
2019-01-17 16:16:28 +0100

    creation des objets Collection,Categorie,Model

--- src/main/java/com/boutique/controller/CollectionController.java
+++ src/main/java/com/boutique/controller/CollectionController.java
@@ -1,10 +1,57 @@
 package com.boutique.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/collections")
-public class CollectionController {
+import com.boutique.dao.CollectionRepository;
+import com.boutique.model.Collection;
 
+@RestController
+public class CollectionController {
+	
+	//------ Repository Collection
+	@Autowired
+	private CollectionRepository cr;
+	
+	//---------- Get All Collection ------------------
+	@GetMapping(path="/getAllCollection")
+	public List<Collection> getAllModel() {		
+		return cr.findAll();
+	}
+	
+	//--------- Get Collection ---------------
+	@GetMapping(path="/getCollectionById/{id}")
+	public Optional<Collection> getModel(@PathVariable("id") Long id) {
+		return cr.findById(id);
+	}
+	
+	//--------- Create Collection ---------------
+	@PostMapping(path="/saveCollection")
+	public Collection saveModele(@RequestBody Collection collection) {
+		return cr.save(collection);
+	}
+	
+	//--------- Update Collection ---------------
+	@PutMapping(path="/updateCollection/{id}")
+	public Collection updateModele(@RequestBody Collection collection) {
+		collection.setIdCollection(collection.getIdCollection());
+		return cr.save(collection);
+	}
+	
+	//--------- Delete Collection ---------------
+	@DeleteMapping(path="/deleteCollection/{id}")
+	public boolean deleteModele(@RequestParam Long id) {
+		cr.deleteById(id);
+		return true;
+	}
 }
--- src/main/java/com/boutique/controller/ModeleController.java
+++ src/main/java/com/boutique/controller/ModeleController.java
@@ -1,10 +1,59 @@
 package com.boutique.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/modeles")
-public class ModeleController {
+import com.boutique.dao.ModeleRepository;
+import com.boutique.model.Modele;
 
+@RestController
+public class ModeleController {
+	
+	
+	//------ Repository Modele
+	@Autowired
+	private ModeleRepository mr;
+	
+	//---------- Get All Modele ------------------
+	@GetMapping(name="/getAllModele")
+	public List<Modele> getAllModel() {		
+		return mr.findAll();
+	}
+	
+	//--------- Get Modele ---------------
+	@GetMapping(path="/getModeleById/{id}")
+	public Optional<Modele> getModel(@PathVariable("id") Long id) {
+		return mr.findById(id);
+	}
+	
+	//--------- Create Modele ---------------
+	@PostMapping(path="/saveModele")
+	public Modele saveModele(@RequestBody Modele modele) {
+		return mr.save(modele);
+	}
+	
+	//--------- Update Modele ---------------
+	@PutMapping(name="/updateModele/{id}")
+	public Modele updateModele(@RequestBody Modele modele) {
+		modele.setIdModel(modele.getIdModel());
+		return mr.save(modele);
+	}
+	
+	//--------- Delete Modele ---------------
+	@DeleteMapping(name="/deleteModele/{id}")
+	public boolean deleteModele(@RequestParam Long id) {
+		mr.deleteById(id);
+		return true;
+	}
 }
